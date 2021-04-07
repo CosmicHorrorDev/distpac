2d8470c3f2212c47021885290041e3d2e4d695e6
Lovecraftian Horror <LovecraftianHorror@pm.me>
2021-04-07 01:34:20 -0400

    Add support for multiple files and serve a file name

--- named-file-server/src/main.rs
+++ named-file-server/src/main.rs
@@ -5,23 +5,25 @@ use std::{
     path::PathBuf,
 };
 
-/// A dead simple http server for serving a single file. The file will be served at the root of
-/// whatever address is specified by `--socket`
+/// A dead simple http server for serving files at whatever address is specified by `--socket`
 #[derive(Clap, Debug)]
 struct Opts {
     /// Specify the socket for the webserver to attempt to bind to
     #[clap(short, long, default_value = "127.0.0.1:9090")]
     socket: SocketAddrV4,
-    /// The path to the file that will be served at the root of the web server
-    file: PathBuf,
+    /// The paths to the files that will be served at the root of the web server
+    files: Vec<PathBuf>,
 }
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let Opts { socket, file } = Opts::parse();
+    let Opts { socket, files } = Opts::parse();
 
     let mut app = tide::new();
-    app.at("/").serve_file(file)?;
+    for file in files {
+        let file_name = file.file_name().unwrap().to_string_lossy();
+        app.at(&format!("/{}", file_name)).serve_file(file)?;
+    }
     app.listen(SocketAddr::from(socket)).await?;
 
     Ok(())
