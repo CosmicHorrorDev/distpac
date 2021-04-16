dedc50ac7a8e4513c36fd2a313b19bf489180894
Lovecraftian Horror <LovecraftianHorror@pm.me>
2021-04-16 15:09:08 -0400

    Add logging to named file server

--- named-file-server/src/main.rs
+++ named-file-server/src/main.rs
@@ -1,10 +1,17 @@
 use clap::Clap;
+use log::debug;
 
 use std::{net::SocketAddr, path::PathBuf};
 
 /// A dead simple http server for serving files at whatever address is specified by `--socket`
 #[derive(Clap, Debug)]
 struct Opts {
+    /// Silence all output
+    #[clap(short, long)]
+    quiet: bool,
+    /// Increase verbosity
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: usize,
     /// Specify the socket for the webserver to attempt to bind to
     #[clap(short, long, default_value = "127.0.0.1:9090")]
     socket: SocketAddr,
@@ -14,7 +21,19 @@ struct Opts {
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
-    let Opts { socket, files } = Opts::parse();
+    let Opts {
+        socket,
+        files,
+        verbose,
+        quiet,
+    } = Opts::parse();
+    stderrlog::new()
+        .quiet(quiet)
+        .verbosity(verbose)
+        .init()
+        .unwrap();
+    debug!("socket: {:?}", socket);
+    debug!("files: {:#?}", files);
 
     let mut app = tide::new();
     for file in files {
