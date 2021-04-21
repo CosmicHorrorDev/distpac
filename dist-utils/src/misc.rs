922e6424a881c939406e7e0b1349b4a34bc077a8
Lovecraftian Horror <LovecraftianHorror@pm.me>
2021-04-21 09:04:47 -0400

    Fix panic

--- dist-utils/src/misc.rs
+++ dist-utils/src/misc.rs
@@ -4,7 +4,8 @@ pub fn stop_process_by_name(name: &str) {
     let mut system = System::new();
     system.refresh_all();
     // Name is truncated here and I don't feel like trying to snag it from the command path
-    let processes = system.get_process_by_name(&name[..15]);
+    let name = if name.len() < 15 { name } else { &name[..15] };
+    let processes = system.get_process_by_name(name);
 
     for process in processes {
         process.kill(Signal::Interrupt);
